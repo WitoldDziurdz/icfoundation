7b6dbe0b91f210f4cb614ad32fa3f2a83bd39156
apietrzak <adam.pietrzak@solwit.com>
2016-09-21 13:57:06 +0200

    Changes

--- src/main/java/com/lorepo/icf/properties/IStaticListProperty.java
+++ src/main/java/com/lorepo/icf/properties/IStaticListProperty.java
@@ -1,18 +1,10 @@
 package com.lorepo.icf.properties;
 
-/**
- * Lista zawierającą pod property. Np. choice zawiera property items
- * określające ilość elementów opcji
- * 
- * @author Krzysztof Langner
- *
- */
 public interface IStaticListProperty extends IProperty {
 
 	public int getChildrenCount();
 	public void addChildren(int count);
 	public IPropertyProvider getChild(int index);
-	public void removeChildren(int index);
 	public void moveChildUp(int index);
 	public void moveChildDown(int index);
 }
